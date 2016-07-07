934ab75dcb4cbc9d0f59915132bb865e2d3da8b9
KW <wenkangjing@gmail.com>
2016-07-07 17:13:30 +1000

    finished example + redux devtools

---
+++ js/sandbox/README.md
@@ -0,0 +1,14 @@
+## References:
+
+https://github.com/reactjs/redux/tree/master/examples
+
+http://toranbillups.com/blog/archive/2016/01/22/redux-for-the-very-beginner/
+
+https://vimeo.com/151603293
+
+## Run
+> npm install
+> 
+> Rename x_xxx.js to index.js
+> 
+> npm start
--- js/sandbox/index.js
+++ js/sandbox/index.js
@@ -1,22 +1,90 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import Counter from './components/Counter'
-import counter from './reducers'
+import {createStore, combineReducers} from 'redux';
+import React from 'react';
+import {render} from 'react-dom';
+import {Provider, connect} from 'react-redux';
 
-const store = createStore(counter)
-const rootEl = document.getElementById('root')
+var low = ((state=0, action) => {
+  if (action.type === 'UP') {
+    return state + 1;
+  }
+  return state;
+});
 
-function render() {
-  ReactDOM.render(
-    <Counter
-      value={store.getState()}
-      onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-      onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
-    />,
-    rootEl  
-  )
+
+var high = ((state=9, action) => {
+  if (action.type === 'DOWN') {
+    return state - 1;
+  }
+  return state;
+});
+
+
+// 1) create the store
+// 2) get the state
+// 3) change the state
+// 4) subscribe to changes
+
+const Basement = (props) => {
+  const state = props.high;
+  return (
+    <div>basement! {state}
+    <button onClick={props.down}>down</button>
+    </div>
+  );
 }
 
-render()
-store.subscribe(render)
+const Middle = () => {
+  return (
+    <div>middle !<XBasement/></div>
+  );
+}
+
+const TopLevel = (props) => {
+  const state = props.low;
+  return (
+    <div> top! {state}
+      <button onClick={props.up}>up</button>
+      <Middle/>
+    </div>
+  );
+}
+
+const mapState = (state) => {
+  return {
+    low: state.low
+  }
+}
+
+const mapDispatch = (dispatch) => {
+  return {
+    up: () => dispatch({type:'UP'})
+  }
+}
+
+const mapStateBasement = (state) => {
+  return {
+    high: state.high
+  }
+}
+
+const mapDispatchBasement = (dispatch) => {
+  return {
+    down: () => dispatch({type:'DOWN'})
+  }
+}
+
+const XTopLevel = connect(mapState, mapDispatch)(TopLevel)
+const XBasement = connect(mapStateBasement, mapDispatchBasement)(Basement)
+const store = createStore(combineReducers({low:low, high:high}), 
+  window.devToolsExtension ? window.devToolsExtension() : undefined);
+
+render((
+  <Provider store={store}>
+    <div>
+      <XTopLevel />
+    </div>
+  </Provider>
+  ),
+  document.getElementById('root')
+);
+
